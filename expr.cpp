081e21f7c3eb68a83c94a3f69be7961af2c2e7f5
Helric122915 <jbb47@zips.uakron.edu>
2017-01-19 19:15:59 +0000

    Initial Commit

---
+++ expr.cpp
@@ -0,0 +1,26 @@
+// Base Class
+struct Expr{
+  virtual ~Expr(){}
+};
+
+struct True_Expr : Expr {};
+
+struct False_Expr : Expr {};
+
+struct Not_Expr : Expr {
+  Expr *e;
+};
+
+struct And_Expr : Expr {
+  Expr *e1;
+  Expr *e2;
+};
+
+struct Or_Expr : Expr {
+  Expr *e1;
+  Expr *e2;
+};
+
+int main(){
+
+}
