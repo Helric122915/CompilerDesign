3bfc02235929914408064db25547ad8a17c4e25c
Helric122915 <jbb47@zips.uakron.edu>
2017-02-02 19:56:32 +0000

    Initial Commit

---
+++ eval.h
@@ -0,0 +1,35 @@
+#include "bool_expr.h"
+#include "int_expr.h"
+
+int eval(Expr *e)
+{
+  class V : public Expr::Visitor {
+    public:
+      int r;
+      void visit(Bool_Expr* e) { r = e->getValue(); }
+      void visit(And_Expr* e) { r = eval(e->getE1()) & eval(e->getE2()); }
+      void visit(Or_Expr* e) { r = eval(e->getE1()) | eval(e->getE2()); }
+      void visit(Xor_Expr* e) { r = eval(e->getE1()) ^ eval(e->getE2()); }
+      void visit(Not_Expr* e) { r = !eval(e->getE()); }
+      void visit(Eq_Expr* e) { r = eval(e->getE1()) == eval(e->getE2()); }
+      void visit(NotEq_Expr* e) { r = eval(e->getE1()) != eval(e->getE2()); }
+      void visit(Cond_Expr* e) { r = (eval(e->getE1())?eval(e->getE2()):eval(e->getE3())); }
+      void visit(AndThen_Expr* e) { r = (eval(e->getE1())?eval(e->getE2()):false); }
+      void visit(OrElse_Expr* e) { r = (eval(e->getE1())?true:eval(e->getE2())); }
+
+      void visit(Int_Expr* e) { r = e->getValue(); }
+      void visit(Add_Expr* e) { r = eval(e->getE1()) + eval(e->getE2()); }
+      void visit(Sub_Expr* e) { r = eval(e->getE1()) - eval(e->getE2()); }
+      void visit(Mult_Expr* e) { r = eval(e->getE1()) * eval(e->getE2()); }
+      void visit(Div_Expr* e) { r = eval(e->getE1()) / eval(e->getE2()); }
+      void visit(Mod_Expr* e) { r = eval(e->getE1()) % eval(e->getE2()); }
+      void visit(LessThan_Expr* e) { r = eval(e->getE1()) < eval(e->getE2()); }
+      void visit(GreaterThan_Expr* e) { r = eval(e->getE1()) > eval(e->getE2()); }
+      void visit(LessEqThan_Expr* e) { r = eval(e->getE1()) <= eval(e->getE2()); }
+      void visit(GreaterEqThan_Expr* e) { r = eval(e->getE1()) >= eval(e->getE2()); }
+      void visit(Negation_Expr *e) { r = 0 - eval(e->getE()); }
+  };
+  V vis;
+  e->accept(vis);
+  return vis.r;
+}
