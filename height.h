b58a6ea6b8cde19e3e34c18cfe19c4e40dd08e6a
Helric122915 <jbb47@zips.uakron.edu>
2017-02-02 15:07:11 -0500

    Update height.h

--- height.h
+++ height.h
@@ -1,1 +1,36 @@
-add
+#include "bool_expr.h"
+#include "int_expr.h"
+#include <algorithm>
+
+int height(Expr *e)
+{
+  class V : public Expr::Visitor {
+    public:
+      int r;
+      void visit(Bool_Expr* e) { r = 1; }
+      void visit(And_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Or_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Xor_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Not_Expr* e) { r = 1 + height(e->getE()); }
+      void visit(Eq_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(NotEq_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Cond_Expr* e) { r = 1 + std::max(height(e->getE1()) + std::max(height(e->getE2() + height(e->getE3()))); }
+      void visit(AndThen_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(OrElse_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+
+      void visit(Int_Expr* e) { r = 1; }
+      void visit(Add_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Sub_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Mult_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Div_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Mod_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(LessThan_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(GreaterThan_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(LessEqThan_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(GreaterEqThan_Expr* e) { r = 1 + std::max(height(e->getE1()) + height(e->getE2())); }
+      void visit(Negation_Expr *e) { r = 1 + height(e->getE()); }
+  };
+  V vis;
+  e->accept(vis);
+  return vis.r;
+}
