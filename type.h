6196f8d642bccd38534df19d727876b3ace1c32a
Helric122915 <jbb47@zips.uakron.edu>
2017-02-02 20:47:08 +0000

    adding visitor pattern

--- type.h
+++ type.h
@@ -2,14 +2,86 @@
 #define TYPE_H
 
 #include <stdexcept>
+#include "context.h"
+#include "expr.h"
 
 class Type {
   public:
-    virtual ~Type() {}
+    virtual ~Type() = default;
 };
 
 class Bool_Type : public Type {};
 class Int_Type : public Type {};
+
+Type* check(Expr* e, ASTcontext cxt) {
+  class V : public Expr::Visitor {
+    ASTcontext& cxt;
+    Type* r;
+
+    V(ASTcontext& cxt) : cxt(cxt) {}
+
+    void visit(Bool_Expr* e) { r = &cxt.Bool_; }
+    void visit(And_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Bool_ && e->getE2()->getType() == &cxt.Bool_)
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+    void visit(Or_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Bool_ && e->getE2()->getType() == &cxt.Bool)
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+    void visit(Xor_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Bool_ && e->getE2()->getType() == &cxt.Bool_)
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+    void visit(Not_Expr* e) {
+      if (e->getE()->getType() == &cxt.Bool_)
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+    void visit(Eq_Expr* e) {
+      if (e->getE1()->getType() == e->getE2()->getType())
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+    void visit(NotEq_Expr* e) {
+      if (e->getE1()->getType() == e->getE2()->getType())
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+    void visit(Cond_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Bool_ && e->getE2()->getType() == e->getE3()->getType())
+        r = e->getE2()->getType();
+      else throw Type_Error;
+    }
+    void visit(AndThen_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Bool_ && e->getE2()->getType() == &cxt.Bool_)
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+    void visit(OrElse_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Bool_ && e->getE2()->getType() == &cxt.Bool_)
+        r = &cxt.Bool_;
+      else throw Type_Error;
+    }
+
+    void visit(Int_Expr* e) { r = &cxt.Int_ }
+    void visit(Add_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Int_ && e->getE2()->getType() == &cxt.Int_)
+        r = &cxt.Int_;
+      else throw Type_Error;
+    }
+    void visit(Sub_Expr* e) {
+      if (e->getE1()->getType() == &cxt.Int_ && e->getE2()->getType() == &cxt.Int_)
+        r = &cxt.Int_;
+      else throw Type_Error;
+    }
+    void visit(Mult_Expr* e) {
+      if (e->
+  };
+}
 
 class Type_Exception : public std::exception {
   public:
